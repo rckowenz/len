3ee831f986a1b9841f4c616072256956f224920d
rckowenz <142201847+rckowenz@users.noreply.github.com>
2023-09-04 08:04:03 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <title>len</title>
+        <meta name="theme-color" content="#313338">
+        <meta property="og:title" content="ينزف>">
+        <meta property="og:description" content="lol">
+        <link rel="stylesheet" href="main.css">
+        <script src="https://heal.rip/script.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <div id="enter-portal" onclick="enterPortal()">kys</div>
+            <div id="body-div" class="hidden">
+                <div class="image-container">
+                </div>
+                <div class="typing-container">
+                    <div id="typing" class="typing-text"></div>
+                    <div id="cursor" class="cursor"></div>
+                    <div class="text-container">
+                        <a class="url-text" href="https://instagram.com/xlainlol" target="_blank">len</a>
+                        <a class="url-text" href="https://discord.gg/sword" target="_blank">kys</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="main.js"></script>
+        <script>
+            document.onkeydown = e=>(e.keyCode === 123 || (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74)) || (e.ctrlKey && e.keyCode === 85)) ? (alert("403 forbidden."),
+            false) : undefined
+        </script>
+    </body>
+</html>
